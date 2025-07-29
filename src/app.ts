c68b80e5470e813a3a1e64874e661eeb14d36a7b
zakariabn <zakaria7262@gmail.com>
2025-07-29 17:07:48 +0600

    global error handler and not-found route crated

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,28 @@
-// const express = require('express')
-
-// import express from "express"
-
-// const app = express()
-// const port = 3000
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
-
 import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { router } from './app/router';
+import { globalErrorHandler } from './app/middleware/globalErrorHandler';
+import notFound from './app/middleware/notFound';
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// routes
+app.use('/api/v1', router);
 
 // root route
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ success: true, message: 'Welcome To Digital Wallet API' });
 });
+
+// global error handler
+app.use(globalErrorHandler);
+
+// 404/Not-found route
+app.use(notFound);
+
+export default app;
